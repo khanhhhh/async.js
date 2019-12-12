8ac932d8da22d603d2188b67a73ea716dfb7d234
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-12-13 01:29:18 +0800

    many tasks

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const {executeAsync} = require("./async.js");
+/*
 // basic
 executeAsync(function(a, b) {
 	return a + b;
@@ -18,10 +19,38 @@ for (let i=0; i<10; i++)
   }));
 
   }, Math.random() * 200 * i);
+*/
 
-
-
-
+// too many tasks
+const num_thread = 4;
+const num_task = 100;
+const data_init = function(range = 0) {
+  let data = [];
+  for (let i=0; i<range; i++)
+    data.push(i);
+  return data;
+};
+let data = data_init(num_task);
+const routine = function(index, item) {
+  return {index, value: 2*item};
+}
+let next_index = 0;
+const routineAsync = function(cb) {
+  executeAsync(routine, next_index, data[next_index]).then(function(out) {
+    //console.log(out);
+    const {index, value} = out;
+    data[index] = value;
+    next_index++;
+    if (next_index < num_task)
+      routineAsync(cb);
+    else if (next_index == num_task + num_thread -1)
+      cb();
+  });
+};
+for (let t=0; t<num_thread; t++)
+  routineAsync(function() {
+    console.log(data);
+  });
 
 
 
