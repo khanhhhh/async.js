2ee3181bedc192b8c727f6065c1cb86011913076
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-12-13 00:58:00 +0800

    counter

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const {executeAsync} = require("./async.js");
-
 // basic
 executeAsync(function(a, b) {
 	return a + b;
 }, 1, 2).then(console.log).catch(console.error);
+
+// counter
+let counter = 0;
+let wait_group = [];
+for (let i=0; i<10; i++)
+  setTimeout(function() {
+
+  wait_group.push(executeAsync(function(counter) {
+    return counter+1;
+  }, counter).then(function(data) {
+    counter = data;
+    console.log(data);
+  }));
+
+  }, Math.random() * 200 * i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
